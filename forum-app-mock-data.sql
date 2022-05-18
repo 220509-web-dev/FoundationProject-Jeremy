fb4735786a80a31ee69d65ae679c118dbd8bd905
Jeremy Smalls-Bushay <arcane1241@gmail.com>
2022-05-18 14:32:30 -0400

    added users mock data

--- forum-app-mock-data.sql
+++ forum-app-mock-data.sql
@@ -5,8 +5,15 @@ insert into user_roles (name) values ('ADMIN'), ('BASIC_USER'), ('VERIFIED_USER'
 
 --create a new Forum user
 insert into app_users (first_name, last_name, email, username, password, profile_pic, role_id)
-values ('Dummy', 'Bot', 'Dummybot@gmail.com', 'DummyUser', 'DummyPass', 
-'https://res.cloudinary.com/drrkccbb4/image/upload/v1652890771/ForumApp/df2srkc-b2e2aa6b-1d68-4ae9-9743-c8559e50c8b1_dufgso.png', 4);
+values 
+    ('Dummy', 'Bot', 'Dummybot@gmail.com', 'DummyUser', 'DummyPassfew', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896084/ForumApp/download_d5mnuu.jpg', 3),
+    ('Jeremy', 'Bushay', 'jbushay@gmail.com', 'Sosathegod', 'sosa1242', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896153/ForumApp/download_livdk3.jpg', 4),
+    ('Hedwig', 'Maldin', 'hmaldin0@parallels.com', 'hmaldin0', 'FynEu4702MSfewc', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896048/ForumApp/images_qgcwsr.jpg', 2),
+    ('Burk', 'Godmer', 'bgodmer1@eventbrite.com', 'bgodmer1', 'g9fnoBefwefgwgw', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896043/ForumApp/images_nbeejd.jpg', 1),
+    ('Blakelee', 'Touhig', 'btouhig2@taobao.com', 'btouhig2', 'uIt8iQfewfewRR', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896039/ForumApp/download_ltn8yu.jpg', 1),
+    ('Salomi', 'Strangward', 'sstrangward3@nature.com', 'sstrangward3', 'tmyhU2UHfwejEN', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896025/ForumApp/download_oaxwh4.jpg', 2),
+    ('Mischa', 'Bowling', 'mbowling4@buzzfeed.com', 'mbowling4', 'CILhLmwfew3Dc', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896022/ForumApp/download_ydvto5.jpg', 4),
+    ('See', 'Haymes', 'shaymes5@tinypic.com', 'shaymes5', 'sBkCt8tfewfews', 'https://res.cloudinary.com/drrkccbb4/image/upload/v1652896001/ForumApp/download_bbewwa.jpg', 4);
 
 --create some categories for the Forum
 insert into categories (category_name) 
